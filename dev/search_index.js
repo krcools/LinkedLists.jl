85876533c894214c33cf001d8b3bbea44902f5b3
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-11 11:10:48 +0000

    build based on 40cf612

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LinkedLists.jl-1","page":"Home","title":"LinkedLists.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [LinkedLists]","category":"page"},{"location":"#LinkedLists.LinkedList-Tuple{Array{T,1} where T}","page":"Home","title":"LinkedLists.LinkedList","text":"LinkedList(collection)\n\nCreates a new linked list from an iterable collection.\n\n\n\n\n\n","category":"method"}]
+}
